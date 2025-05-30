aacfa35e911d123a6ab08c368a046e17a40d7875
Kalilou Diaby <kalilou@revolight.io>
2025-05-30 15:21:06 +0200

    threadpilot-insurance-TK-03: Remove the contract file not being used and update the controller unittests

--- src/test/java/com/threadpilot/insurance/controller/InsuranceControllerTest.java
+++ src/test/java/com/threadpilot/insurance/controller/InsuranceControllerTest.java
@@ -26,136 +26,156 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class InsuranceControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @MockBean
-    private InsuranceService insuranceService;
+        @MockBean
+        private InsuranceService insuranceService;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+        @Autowired
+        private ObjectMapper objectMapper;
 
-    Insurance insurance;
-    InsuranceResponse insuranceResponse;
-    Vehicle vehicle;
-    Promotion promotion;
+        Insurance insurance;
+        InsuranceResponse insuranceResponse;
+        Vehicle vehicle;
+        Promotion promotion;
 
-    @BeforeEach
-    void setup() {
+        @BeforeEach
+        void setup() {
 
-        insurance = Insurance.builder()
-                .insuranceNumber("1234567890")
-                .insuranceType("PetInsurance")
-                .insurancePrice(10)
-                .discount(0)
-                .insuranceStartDate(Date.from(LocalDate.parse("2025-01-01").atStartOfDay().toInstant(ZoneOffset.UTC)))
-                .insuranceEndDate(Date.from(LocalDate.parse("2025-12-31").atStartOfDay().toInstant(ZoneOffset.UTC)))
-                .insuranceStatus("Active")
-                .insuranceOwnerNumber("199001011235")
-                .build();
+                insurance = Insurance.builder()
+                                .insuranceNumber("1234567890")
+                                .insuranceType("PetInsurance")
+                                .insurancePrice(10)
+                                .discount(0)
+                                .insuranceStartDate(Date.from(
+                                                LocalDate.parse("2025-01-01").atStartOfDay().toInstant(ZoneOffset.UTC)))
+                                .insuranceEndDate(Date.from(
+                                                LocalDate.parse("2025-12-31").atStartOfDay().toInstant(ZoneOffset.UTC)))
+                                .insuranceStatus("Active")
+                                .insuranceOwnerNumber("199001011235")
+                                .build();
 
-        insuranceResponse = InsuranceResponse.builder()
-                .insurances(List.of(insurance))
-                .insuranceOwnerNumber(insurance.getInsuranceOwnerNumber())
-                .build();
+                insuranceResponse = InsuranceResponse.builder()
+                                .insurances(List.of(insurance))
+                                .insuranceOwnerNumber(insurance.getInsuranceOwnerNumber())
+                                .build();
 
-        vehicle = Vehicle.builder()
-                .registrationNumber("1234567890")
-                .model("Car")
-                .make("Toyota")
-                .year(2020)
-                .color("Red")
-                .ownerPersonalNumber("199001011235")
-                .mileage(1000L)
-                .build();
-    }
+                vehicle = Vehicle.builder()
+                                .registrationNumber("1234567890")
+                                .model("Car")
+                                .make("Toyota")
+                                .year(2020)
+                                .color("Red")
+                                .ownerPersonalNumber("199001011235")
+                                .mileage(1000L)
+                                .build();
+        }
 
-    // Get Controller Test
+        // Get Controller Test
 
-    @Order(1)
-    @Test
-    void testGetInsurances() throws Exception {
-        String insuranceOwnerNumber = "199001011235";
-        given(insuranceService.getInsurancesByOwnerNumber(insuranceOwnerNumber)).willReturn(insuranceResponse);
+        @Order(1)
+        @Test
+        void testGetInsurances() throws Exception {
+                String insuranceOwnerNumber = "199001011235";
+                given(insuranceService.getInsurancesByOwnerNumber(insuranceOwnerNumber)).willReturn(insuranceResponse);
 
-        ResultActions response = mockMvc.perform(get("/api/insurances/{insuranceOwnerNumber}", insuranceOwnerNumber)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(insuranceResponse)));
+                ResultActions response = mockMvc
+                                .perform(get("/api/insurances/{insuranceOwnerNumber}", insuranceOwnerNumber)
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(objectMapper.writeValueAsString(insuranceResponse)));
 
-        System.out.println(response);
+                response.andDo(print()).andExpect(status().isOk())
+                                .andExpect(jsonPath("$[0].insuranceOwnerNumber").value(insuranceOwnerNumber))
+                                .andExpect(jsonPath("$[0].insurances[0].insuranceNumber")
+                                                .value(insurance.getInsuranceNumber()))
+                                .andExpect(jsonPath("$[0].insurances[0].insuranceType")
+                                                .value(insurance.getInsuranceType()))
+                                .andExpect(jsonPath("$[0].insurances[0].insurancePrice")
+                                                .value(insurance.getInsurancePrice()))
+                                .andExpect(jsonPath("$[0].insurances[0].discount").value(insurance.getDiscount()))
+                                .andExpect(jsonPath("$[0].insurances[0].insuranceStartDate")
+                                                .value("2025-01-01T00:00:00.000+00:00"))
+                                .andExpect(jsonPath("$[0].insurances[0].insuranceEndDate")
+                                                .value("2025-12-31T00:00:00.000+00:00"))
+                                .andExpect(jsonPath("$[0].insurances[0].insuranceStatus")
+                                                .value(insurance.getInsuranceStatus()));
+        }
 
-        response.andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].insuranceOwnerNumber").value(insuranceOwnerNumber))
-                .andExpect(jsonPath("$[0].insurances[0].insuranceNumber").value(insurance.getInsuranceNumber()))
-                .andExpect(jsonPath("$[0].insurances[0].insuranceType").value(insurance.getInsuranceType()))
-                .andExpect(jsonPath("$[0].insurances[0].insurancePrice").value(insurance.getInsurancePrice()))
-                .andExpect(jsonPath("$[0].insurances[0].discount").value(insurance.getDiscount()))
-                .andExpect(jsonPath("$[0].insurances[0].insuranceStartDate").value("2025-01-01T00:00:00.000+00:00"))
-                .andExpect(jsonPath("$[0].insurances[0].insuranceEndDate").value("2025-12-31T00:00:00.000+00:00"))
-                .andExpect(jsonPath("$[0].insurances[0].insuranceStatus").value(insurance.getInsuranceStatus()));
-    }
+        @Order(2)
+        @Test
+        void testGetInsuranceWithVehicleInfo() throws Exception {
+                String insuranceOwnerNumber = "199001011235";
 
-    @Order(2)
-    @Test
-    void testGetInsuranceWithVehicleInfo() throws Exception {
-        String insuranceOwnerNumber = "199001011235";
+                insuranceResponse = InsuranceResponse.builder()
+                                .insurances(List.of(insurance))
+                                .insuranceOwnerNumber(insurance.getInsuranceOwnerNumber())
+                                .vehicles(List.of(vehicle))
+                                .build();
 
-        insuranceResponse = InsuranceResponse.builder()
-                .insurances(List.of(insurance))
-                .insuranceOwnerNumber(insurance.getInsuranceOwnerNumber())
-                .vehicles(List.of(vehicle))
-                .build();
+                given(insuranceService.getInsurancesByOwnerNumber(insuranceOwnerNumber)).willReturn(insuranceResponse);
 
-        given(insuranceService.getInsurancesByOwnerNumber(insuranceOwnerNumber)).willReturn(insuranceResponse);
+                ResultActions response = mockMvc
+                                .perform(get("/api/insurances/{insuranceOwnerNumber}", insuranceOwnerNumber)
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(objectMapper.writeValueAsString(insuranceResponse)));
 
-        ResultActions response = mockMvc.perform(get("/api/insurances/{insuranceOwnerNumber}", insuranceOwnerNumber)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(insuranceResponse)));
+                response.andDo(print()).andExpect(status().isOk())
+                                .andExpect(jsonPath("$[0].insuranceOwnerNumber").value(insuranceOwnerNumber))
+                                .andExpect(jsonPath("$[0].insurances[0].insuranceNumber")
+                                                .value(insurance.getInsuranceNumber()))
+                                .andExpect(jsonPath("$[0].insurances[0].insuranceType")
+                                                .value(insurance.getInsuranceType()))
+                                .andExpect(jsonPath("$[0].insurances[0].insurancePrice")
+                                                .value(insurance.getInsurancePrice()))
+                                .andExpect(jsonPath("$[0].insurances[0].discount").value(insurance.getDiscount()))
+                                .andExpect(jsonPath("$[0].insurances[0].insuranceStartDate")
+                                                .value("2025-01-01T00:00:00.000+00:00"))
+                                .andExpect(jsonPath("$[0].insurances[0].insuranceEndDate")
+                                                .value("2025-12-31T00:00:00.000+00:00"))
+                                .andExpect(jsonPath("$[0].insurances[0].insuranceStatus")
+                                                .value(insurance.getInsuranceStatus()))
+                                .andExpect(jsonPath("$[0].vehicles[0].registrationNumber")
+                                                .value(vehicle.getRegistrationNumber()))
+                                .andExpect(jsonPath("$[0].vehicles[0].model").value(vehicle.getModel()))
+                                .andExpect(jsonPath("$[0].vehicles[0].make").value(vehicle.getMake()))
+                                .andExpect(jsonPath("$[0].vehicles[0].year").value(vehicle.getYear()))
+                                .andExpect(jsonPath("$[0].vehicles[0].color").value(vehicle.getColor()))
+                                .andExpect(jsonPath("$[0].vehicles[0].ownerPersonalNumber")
+                                                .value(vehicle.getOwnerPersonalNumber()))
+                                .andExpect(jsonPath("$[0].vehicles[0].mileage").value(vehicle.getMileage()));
+        }
 
-        response.andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].insuranceOwnerNumber").value(insuranceOwnerNumber))
-                .andExpect(jsonPath("$[0].insurances[0].insuranceNumber").value(insurance.getInsuranceNumber()))
-                .andExpect(jsonPath("$[0].insurances[0].insuranceType").value(insurance.getInsuranceType()))
-                .andExpect(jsonPath("$[0].insurances[0].insurancePrice").value(insurance.getInsurancePrice()))
-                .andExpect(jsonPath("$[0].insurances[0].discount").value(insurance.getDiscount()))
-                .andExpect(jsonPath("$[0].insurances[0].insuranceStartDate").value("2025-01-01T00:00:00.000+00:00"))
-                .andExpect(jsonPath("$[0].insurances[0].insuranceEndDate").value("2025-12-31T00:00:00.000+00:00"))
-                .andExpect(jsonPath("$[0].insurances[0].insuranceStatus").value(insurance.getInsuranceStatus()))
-                .andExpect(jsonPath("$[0].vehicles[0].registrationNumber").value(vehicle.getRegistrationNumber()))
-                .andExpect(jsonPath("$[0].vehicles[0].model").value(vehicle.getModel()))
-                .andExpect(jsonPath("$[0].vehicles[0].make").value(vehicle.getMake()))
-                .andExpect(jsonPath("$[0].vehicles[0].year").value(vehicle.getYear()))
-                .andExpect(jsonPath("$[0].vehicles[0].color").value(vehicle.getColor()))
-                .andExpect(jsonPath("$[0].vehicles[0].ownerPersonalNumber").value(vehicle.getOwnerPersonalNumber()))
-                .andExpect(jsonPath("$[0].vehicles[0].mileage").value(vehicle.getMileage()));
-    }
+        @Order(3)
+        @Test
+        void testGetInsuranceWithPromotionBlackFriday() throws Exception {
+                String insuranceOwnerNumber = "199001011235";
+                promotion = Promotion.builder()
+                                .description("Black Friday")
+                                .discount(10)
+                                .validity(LocalDate.parse("2025-12-31"))
+                                .build();
 
-    @Order(3)
-    @Test
-    void testGetInsuranceWithPromotionBlackFriday() throws Exception {
-        String insuranceOwnerNumber = "199001011235";
-        promotion = Promotion.builder()
-                .description("Black Friday")
-                .discount(10)
-                .validity(LocalDate.parse("2025-12-31"))
-                .build();
+                insuranceResponse = InsuranceResponse.builder()
+                                .insurances(List.of(insurance))
+                                .insuranceOwnerNumber(insurance.getInsuranceOwnerNumber())
+                                .promotion("Black Friday - 10% off, valid until 2025-12-31")
+                                .build();
 
-        insuranceResponse = InsuranceResponse.builder()
-                .insurances(List.of(insurance))
-                .insuranceOwnerNumber(insurance.getInsuranceOwnerNumber())
-                .promotion("Black Friday - 10% off, valid until 2025-12-31")
-                .build();
+                given(insuranceService.getInsurancesByOwnerNumber(insuranceOwnerNumber)).willReturn(insuranceResponse);
 
-        given(insuranceService.getInsurancesByOwnerNumber(insuranceOwnerNumber)).willReturn(insuranceResponse);
+                ResultActions response = mockMvc
+                                .perform(get("/api/insurances/{insuranceOwnerNumber}", insuranceOwnerNumber)
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(objectMapper.writeValueAsString(insuranceResponse)));
 
-        ResultActions response = mockMvc.perform(get("/api/insurances/{insuranceOwnerNumber}", insuranceOwnerNumber)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(insuranceResponse)));
-
-        response.andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].insuranceOwnerNumber").value(insuranceOwnerNumber))
-                .andExpect(jsonPath("$[0].insurances[0].insuranceNumber").value(insurance.getInsuranceNumber()))
-                .andExpect(jsonPath("$[0].insurances[0].insuranceStatus").value(insurance.getInsuranceStatus()))
-                .andExpect(jsonPath("$[0].promotion").value("Black Friday - 10% off, valid until 2025-12-31"));
-    }
+                response.andDo(print()).andExpect(status().isOk())
+                                .andExpect(jsonPath("$[0].insuranceOwnerNumber").value(insuranceOwnerNumber))
+                                .andExpect(jsonPath("$[0].insurances[0].insuranceNumber")
+                                                .value(insurance.getInsuranceNumber()))
+                                .andExpect(jsonPath("$[0].insurances[0].insuranceStatus")
+                                                .value(insurance.getInsuranceStatus()))
+                                .andExpect(jsonPath("$[0].promotion")
+                                                .value("Black Friday - 10% off, valid until 2025-12-31"));
+        }
 }
